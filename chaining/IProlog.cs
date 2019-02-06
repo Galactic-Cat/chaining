7e610c241c107aa0414988deaaefd8900e1db7b6
Galactic-Cat <simonvanhus@live.nl>
2019-02-06 23:30:03 +0100

    Code is messy and bloated, needs deflation.

---
+++ chaining/IProlog.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chaining
+{
+    interface IProlog
+    {
+        string[] Parents { get; }
+
+        bool? Evaluate(Dictionary<string, Clause> knowledgeBase, bool shallow);
+    }
+
+    class Fact : IProlog
+    {
+        public string[] Parents { get; private set; } = new string[0];
+        private bool State { get; set; } = true;
+
+        public Fact() { }
+        public Fact(bool state)
+        {
+            State = state;
+        }
+    
+        public bool? Evaluate(Dictionary<string, Clause> knowledgeBase, bool shallow = true)
+        {
+            return State;
+        }
+    }
+
+    class Rule : IProlog
+    {
+        private bool? State { get; set; } = null;
+        public string[] Parents { get; private set; }
+
+        public Rule(string[] parents)
+        {
+            Parents = parents;
+        }
+
+        public bool? Evaluate(Dictionary<string, Clause> knowledgeBase, bool shallow = false)
+        {
+            // Return the state if already found
+            if (State != null || shallow)
+                return State;
+
+            // Evaluate the parents
+            foreach (string parent in Parents)
+            {
+                if (knowledgeBase[parent] == null)
+                {
+                    knowledgeBase.Add(parent, new Clause(new IProlog[1] { new Fact(false) }));
+                    State = false;
+                    return false;
+                }
+
+                bool? evaluation = knowledgeBase[parent].Evaluate(knowledgeBase, true);
+                if (evaluation == false)
+                {
+                    State = false;
+                    return false;
+                }
+                else if (evaluation == null)
+                    return null;
+            }
+
+            // All parents were true, so this rule is also true
+            State = true;
+            return true;
+        }
+    }
+
+    class Clause : IProlog
+    {
+        private bool? State { get; set; } = null;
+        public string[] Parents { get; private set; }
+        private List<IProlog> SubClauses { get; set; }
+
+        public Clause(IProlog[] subClauses)
+        {
+            List<string> parents = new List<string>();
+            foreach (IProlog subClause in SubClauses)
+                parents.Concat(subClause.Parents);
+            Parents = parents.ToArray();
+            SubClauses = new List<IProlog>(subClauses);
+        }
+
+        public void Add(IProlog subClause)
+        {
+            SubClauses.Add(subClause);
+        }
+
+        public bool? Evaluate(Dictionary<string, Clause> knowledgeBase, bool shallow = false)
+        {
+            if (State != null || shallow)
+                return State;
+            
+            foreach (IProlog subClause in SubClauses)
+            {
+                bool? evaluation = subClause.Evaluate(knowledgeBase, false);
+                if (evaluation == true)
+                {
+                    State = true;
+                    return true;
+                }
+                else if (evaluation == false)
+                {
+                    State = false;
+                    return false;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- chaining/Program.cs
+++ chaining/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,91 @@ namespace chaining
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Clause> knowledgeBase = new Dictionary<string, Clause>();
+            string[] firstLine = Regex.Match(Console.ReadLine(), @"[0-9]+ [0-9]+ [A-Za-z]").ToString().Split(' ');
+            int clauseCount = int.Parse(firstLine[0]);
+            int queryCount = int.Parse(firstLine[1]);
+            bool forward = (firstLine[2] == "f");
+
+            string[] rawKnowledgeBase = new string[clauseCount];
+            for (int i = 0; i < clauseCount; i++)
+            {
+                string line = Regex.Replace(Console.ReadLine().Split('%')[0], @"\s", ""); // Read and cleanup line
+                if (!line.Contains(".")) do
+                    {
+                        line += Regex.Replace(Console.ReadLine().Split('%')[0], @"\s", ""); // Extend line until a '.' is found
+                    } while (!line.Contains("."));
+                rawKnowledgeBase[i] = line;
+            }
+            string[] facts = ParseKnowledgeBase(knowledgeBase, rawKnowledgeBase); // Parses knowledgebase and returns all facts for forward chaining.
+
+            if (forward)
+                for (int i = 0; i < queryCount; i++)
+                {
+                    string query = Regex.Match(Console.ReadLine(), @"\w+").ToString();
+                    Queue<string> agenda = new Queue<string>(facts);
+                    while (agenda.Count > 0)
+                    {
+                        string p = agenda.Dequeue();
+                        if (p == query)
+                        {
+                            Console.WriteLine(query + ". true.");
+                            break;
+                        }
+
+                        if (knowledgeBase[p].Evaluate())
+                    }
+                }
+        }
+
+        static string[] ParseKnowledgeBase(Dictionary<string, Clause> knowledgeBase, string[] rawKnowledgeBase)
+        {
+            List<string> facts = new List<string>();
+            foreach (string line in rawKnowledgeBase)
+            {
+                if (Regex.IsMatch(line, @"^\w+\.$")) // This line contains a fact
+                {
+                    string name = Regex.Match(line, @"\w+").ToString();
+                    if (knowledgeBase[name] == null)
+                        knowledgeBase.Add(name, new Clause(new IProlog[1] { new Fact() }));
+                    else
+                        knowledgeBase[name].Add(new Fact());
+                    facts.Add(name);
+                    continue;
+                }
+                else // This line contains a rule
+                {
+                    string[] splitLine = Regex.Split(line, ":-");
+                    string name = splitLine[0];
+                    string[] parents = Regex.Split(splitLine[1], @"[\.,]");
+                    if (knowledgeBase[name] == null)
+                        knowledgeBase.Add(name, new Clause(new IProlog[1] { new Rule(parents) }));
+                    else
+                        knowledgeBase[name].Add(new Rule(parents));
+                }
+            }
+            return facts.ToArray();
+        }
+
+        static string[] FindChildren(string[] names, Dictionary<string, IProlog[]> knowledgeBase)
+        {
+            List<string> children = new List<string>();
+            foreach (string key in knowledgeBase.Keys)
+            {
+                if (!names.Contains(key))
+                    foreach (IProlog prolog in knowledgeBase[key])
+                    {
+                        foreach (string parent in prolog.Parents)
+                            if (names.Contains(parent))
+                            {
+                                children.Add(key);
+                                break;
+                            }
+                        if (children.Contains(key))
+                            break;
+                    }
+            }
+            return children.ToArray();
         }
     }
 }
